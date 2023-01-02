27b077b0f8c5babe677e5fcf48bd7970327abc7c
Matteo Piccini <gnappo.1@live.it>
2023-01-02 02:38:07 -0800

    Done, change test to have sorted array or sort in solution

--- lib/email_address_parser.py
+++ lib/email_address_parser.py
@@ -1,1 +1,12 @@
-# your code goes here!
+import re
+
+class EmailAddressParser:
+    def __init__(self, string):
+        self.sentence = string
+    
+    def parse(self):
+        # import ipdb; ipdb.set_trace()
+        return re.split(r"\s|,\s*", self.sentence)
+
+parser = EmailAddressParser("talk@talk.com john.jones@flatironschool.com alexa@amazon.com")
+parser.parse()
